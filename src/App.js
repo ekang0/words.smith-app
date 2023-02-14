14bbffd21222d9f92a189be494aac64211bb63ff
Esther Kang <eskang87@gmail.com>
2023-02-13 19:39:09 -0800

    Add Header NavBar and Instructions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import NavBar from "./NavBar";
+import Instructions from "./Instructions";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <NavBar />
+      <Instructions />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
