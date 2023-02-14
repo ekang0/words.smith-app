09160a7811ea08e7e50c9170c096eb1efe9cca74
Esther Kang <eskang87@gmail.com>
2023-02-14 14:23:43 -0800

    Install react-router-dom Add Router, Switch, and Route to App Add NavLink for instructions and styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,32 @@
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
+  const navStyles = {
+
+  };
+
+  const activeStyles = {
+    background: "#e3d9c3",
+    fontWeight: "bold"
+  };
+
+  const linkStyles = {
+    textDecoration: "none",
+    color: "white",
+    background: "#384b96",
+    display: "inline-block",
+    width: "50%",
+    padding: "20px 0",
+    fontSize: "30px",
+    letterSpacing: "10px",
+    textAlign: "center",
+    lineHeight: "1"
+  };
 
   return (
-    <div>NAVBAR TEST</div>
+    <div className="navBar" style={navStyles}>
+      <NavLink to="/wordssmith/instructions" style={linkStyles} activeStyle={activeStyles}>How to Play</NavLink>
+    </div>
   )
 };
 
